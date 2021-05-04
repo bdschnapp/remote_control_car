0ebf18372f5817e265dca93dc42b80e9760d1204
BenSchnapp <bdschnapp@gmail.com>
2021-05-04 13:39:52 -0700

    esc_calibration bug fix

--- src/esc_calibration.py
+++ src/esc_calibration.py
@@ -5,7 +5,7 @@ MOTOR_PIN = 4
 motor = PWMOutputDevice(MOTOR_PIN, 50)
 
 while True:
-    val = input("Duty Cycle: ")
+    val = int(input("Duty Cycle: "))
     if val == -100:
         break
     motor.value = val

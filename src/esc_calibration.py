93b121122e711bd33b73fcb4177b6d2681e4dda4
BenSchnapp <bdschnapp@gmail.com>
2021-05-07 14:29:24 -0700

    Switched to DC motor with L298N motor controller

--- src/esc_calibration.py
+++ src/esc_calibration.py
@@ -1,12 +1,40 @@
-from gpiozero import PWMOutputDevice
+from gpiozero import PWMOutputDevice, DigitalOutputDevice
 import time
 
-MOTOR_PIN = "GPIO4"
-motor = PWMOutputDevice(pin=MOTOR_PIN, frequency=50)
+IN1 = "GPIO17"  # pin 11
+IN2 = "GPIO27"  # pin 13
+ENA = "GPIO22"  # pin 15
+motor = PWMOutputDevice(pin=ENA, frequency=50)
+in_1 = DigitalOutputDevice(pin=IN1)
+in_2 = DigitalOutputDevice(pin=IN2)
+
+
+def direction_control(direction):
+    motor.value = 0
+    time.sleep(0.01)
+
+    if direction:
+        in_1.on()
+        in_2.off()
+    else:
+        in_1.off()
+        in_2.on()
+
 
 while True:
     val = float(input("Duty Cycle: "))
     if val == -100:
+        motor.value = 0
+        in_1.off()
+        in_2.off()
         break
-    motor.value = val
-    time.sleep(.5)
+
+    if val == -1:
+        direction_control(0)
+
+    if val == -2:
+        direction_control(1)
+
+    else:
+        motor.value = val
+        time.sleep(.5)
